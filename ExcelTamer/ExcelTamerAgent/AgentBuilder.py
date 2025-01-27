3a05e1ee7e6ac1c1f93ff25fd38ddcd924055379
Shamit Verma <github@shamit.in>
2025-01-27 09:50:48 +0000

    Invoke constructor in executor thread

--- ExcelTamer/ExcelTamerAgent/AgentBuilder.py
+++ ExcelTamer/ExcelTamerAgent/AgentBuilder.py
@@ -3,6 +3,7 @@ from concurrent.futures import ThreadPoolExecutor
 from langchain.agents import initialize_agent, AgentType
 from langchain_core.language_models import BaseChatModel
 
+
 from ExcelTamer.ExcelAutomation import ExcelAutomation
 from ExcelTamer.ExcelTamerAgent.ExcelTamerTools import ExcelGetStructureTool
 
@@ -22,8 +23,10 @@ def create_agent(excel_path: str, llm:BaseChatModel) :
 
     global executor
     if executor is None:
-        executor = ThreadPoolExecutor()
-    excel:ExcelAutomation = ExcelAutomation(file_path=excel_path)
+        executor = ThreadPoolExecutor(max_workers=1)
+
+    future = executor.submit(ExcelAutomation, file_path=excel_path)
+    excel:ExcelAutomation = future.result()
     return initialize_agent(
         tools=[ExcelGetStructureTool(excel_automation=excel, executor=executor)],
         llm=llm,
